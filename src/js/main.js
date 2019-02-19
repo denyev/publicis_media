4c712255515dec71f4d9d3c837df82beb191fa74
denyev <denevt87@gmail.com>
2019-02-19 03:22:43 +0300

    Adds styles for menu, header and intro section.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-import './vendor';
+var navMain = document.querySelector('.navigation');
+var navToggle = document.querySelector('.navigation__toggle');
+
+navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('navigation--closed')) {
+        navMain.classList.remove('navigation--closed');
+        navMain.classList.add('navigation--opened');
+    } else {
+        navMain.classList.add('navigation--closed');
+        navMain.classList.remove('navigation--opened');
+    }
+});

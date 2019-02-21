7b69dcb700f4450a6d9c11f9d3406f2a0d2d305a
denyev <denevt87@gmail.com>
2019-02-21 09:11:51 +0300

    Adds a handler for the `Показать ещё` button.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+// Nav menu
+
 var navMain = document.querySelector('.navigation');
 var navToggle = document.querySelector('.navigation__toggle');
 
@@ -9,4 +11,77 @@ navToggle.addEventListener('click', function() {
         navMain.classList.add('navigation--closed');
         navMain.classList.remove('navigation--opened');
     }
+});
+
+// Load more
+
+var mobileScreenWidth = window.matchMedia('(min-width: 320px)');
+
+var hideItems = function(parent, items, loadMoreBtn, maxItems) {
+    var parent = document.querySelector(parent);
+    var items = parent.querySelectorAll(items);
+    var loadMoreBtn = document.querySelector(loadMoreBtn);
+    var hiddenClass = "visually-hidden";
+
+    [].forEach.call(items, function (item, index) {
+        if (index > maxItems - 1) {
+            item.classList.add(hiddenClass);
+        }
+    });
+
+    loadMoreBtn.addEventListener('click', function () {
+        [].forEach.call(document.querySelectorAll('.' + hiddenClass), function (item, index) {
+            console.log(item);
+            if (index < maxItems - 1) {
+                item.classList.remove(hiddenClass);
+            }
+
+            // if (document.querySelectorAll('.' + hiddenClass).length === 0) {
+            //     loadMoreBtn.style.display = 'none';
+            // }
+        });
+    });
+};
+
+var showItems = function(parent, items, maxItems) {
+    var parent = document.querySelector(parent);
+    var items = parent.querySelectorAll(items);
+    var hiddenClass = "visually-hidden";
+
+    [].forEach.call(items, function (item, index) {
+            item.classList.remove(hiddenClass);
+    });
+};
+
+var toggleSlider = function() {
+    if (mobileScreenWidth.matches) {
+        showItems('.clients__list', '.clients__item');
+        console.log("Screen width is at least 320px");
+    } else {
+        hideItems(
+            '.clients__list',
+            '.clients__item',
+            '.clients__more',
+            6
+        );
+        console.log("Screen less than 320px");
+    }
+};
+
+toggleSlider();
+
+window.addEventListener('resize', function() {
+    // if (mobileScreenWidth.matches) {
+    //     showItems('.clients__list', '.clients__item');
+    //     console.log("Screen width is at least 320px");
+    // } else {
+    //     hideItems(
+    //         '.clients__list',
+    //         '.clients__item',
+    //         '.clients__more',
+    //         6
+    //     );
+    //     console.log("Screen less than 320px");
+    // }
+    toggleSlider();
 });
